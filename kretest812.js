c8e613318f584e8750ffa11db29f6468e6fbef77
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-07-14 14:24:01 +0530

    retry after commit failed

--- kretest812.js
+++ kretest812.js
@@ -1,4 +1,26 @@
+const google = require('googleapis').google;
+const _auth = require('./Authorizer');
+const datastore = google.datastore('v1');
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    datastore.projects.beginTransaction({
+        projectId: process.env.GCP_PROJECT,
+        resource: {
+            transactionOptions: {
+                readWrite: {}
+            }
+        }
+    }).then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "transaction": "<transaction ID>"
+        }
+        */
+    })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+    return { "message": "Successfully executed" };
 };
